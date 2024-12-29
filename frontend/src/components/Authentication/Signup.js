b09fcdb64e72aaf0460ed230750eea6e5969a323
Hanxi Zhang <hanxiz@andrew.cmu.edu>
2024-12-28 20:02:22 -0500

    added the frontend code to signup

---
+++ frontend/src/components/Authentication/Signup.js
@@ -0,0 +1,83 @@
+import { VStack } from '@chakra-ui/react'
+
+import React, { useState } from 'react'
+
+import { Button, Fieldset, Input, Stack } from "@chakra-ui/react"
+import { Field } from "../ui/field"
+
+const postDetails = (pics) => {
+
+}
+
+const submitHandler = () => {
+
+}
+
+const Signup = () => {
+    const [name, setName] = useState()
+    const [password, setPassword] = useState()
+    const [confirmpassword, setConfirmpassword] = useState()
+    const [pic, setPic] = useState()
+    const [email, setEmail] = useState()
+
+    return (
+        <VStack color="black">
+             <Fieldset.Root size="lg" maxW="md">
+            <Stack>
+                <Fieldset.Legend>Personal information</Fieldset.Legend>
+                <Fieldset.HelperText>
+                Please provide your information below.
+                </Fieldset.HelperText>
+            </Stack>
+
+            <Fieldset.Content>
+                <Field label="Name">
+                    <Input name="name"
+                        placeholder='Enter Your Name'
+                        required
+                        onChange={(e) => setName(e.target.value)}/>
+                </Field>
+
+                <Field label="Email">
+                    <Input name="email"
+                        placeholder='Enter Your Email'
+                        type="email"
+                        required
+                        onChange={(e) => setEmail(e.target.value)}/>
+                </Field>
+                    
+                <Field label="Password">     
+                    <Input name="password"
+                        placeholder='Enter Your Password'
+                        type="password"
+                        required
+                        onChange={(e) => setPassword(e.target.value)}/>
+                </Field>
+                    
+                <Field label="ConfirmPassword">     
+                    <Input name="confirmpassword"
+                        placeholder='Confirm Your Password'
+                        type="password"
+                        required
+                        onChange={(e) => setConfirmpassword(e.target.value)}/>
+                </Field>
+                    
+            </Fieldset.Content>
+                
+            <Fieldset.Content>
+                    <Input name="Upload Your Profile picture"
+                        p={1.5}
+                        type="file"
+                        accept='image/*'
+                        onChange={(e) => postDetails(e.target.files[0])}/>
+            </Fieldset.Content>
+
+            </Fieldset.Root>
+            <Button type="submit"  _hover={{ bg: "blue.600" }} colorScheme="blue" alignSelf="center" width="100%" onClick={submitHandler}>
+                Sign Up
+            </Button>
+        </VStack>
+    )
+}
+
+export default Signup
--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,8 +1,40 @@
+import { Container, Box, Text, Tabs } from '@chakra-ui/react'
 import React from 'react'
+import Login from "../components/Authentication/Login"
+import Signup from '../components/Authentication/Signup'
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <Container maxW="xl" centerContent>
+          <Box
+              display="flex"
+              justifyContent="center"
+              p={3}
+              bg={"white"}
+              w="100%"
+              m="40px 0 15px 0"
+              borderRadius="lg"
+              borderWidth="1px"
+          >
+              <Text fontSize="4xl" fontFamily="Work sans" color='black'>
+                  Talk-A-Live
+              </Text>
+          </Box>     
+          <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+            <Tabs.Root variant="enclosed" maxW="md" fitted defaultValue={"tab-1"}>
+                <Tabs.List mb="1em">
+                    <Tabs.Trigger width="50%" value="tab-1">Login</Tabs.Trigger>
+                    <Tabs.Trigger width="50%" value="tab-2">Sign Up</Tabs.Trigger>
+                  </Tabs.List>
+                    <Tabs.Content value="tab-1">
+                        <Login/>
+                    </Tabs.Content>
+                    <Tabs.Content value="tab-2">
+                        <Signup />
+                    </Tabs.Content>
+            </Tabs.Root>
+          </Box>
+    </Container>
   )
 }
 
