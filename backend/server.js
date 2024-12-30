3281e2baf97336b018ddde95d25065059c04ed19
HanxiZhang9910 <linsu1702@gmail.com>
2024-12-29 21:53:21 -0500

    setting up the registration process and tested user registration with postman

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes")
 
 dotenv.config()
 connectDB()
@@ -9,19 +10,13 @@ connectDB()
 
 const app = express()
 
+app.use(express.json()) // to accept json data
+
 app.get('/', (req, res) => {
     res.send("API is Running Successfully");
 })
 
-app.get('/api/chat', (req, res) => {
-    res.send(chats);
-})
-
-app.get('/api/chat/:id', (req, res) => {
-    // console.log(req);
-    const singleChat = chats.find((c) => c._id === req.params.id);
-    res.send(singleChat);
-})
+app.use('/api/user', userRoutes)
 
 const PORT = process.env.PORT || 5000
 
